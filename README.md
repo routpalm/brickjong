bfaa039970bb20f6c22082048abe174db3986615
routpalm <nanthon7@uoregon.edu>
2024-10-30 18:14:55 -0700

    deleting stuff

--- README.md
+++
@@ -1,5 +0,0 @@
-# VisuaLoom
-by Brickjong
-Class project for UOCS 422
-
-
--- pages/_app.js
+++
@@ -1,7 +0,0 @@
-import '../styles/view.css';
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
