915dfa5c1c1cfb1b39f0fdefad71b45f467988ea
Tong Guan <yuanyuanbingzi@gmail.com>
2024-11-07 14:01:45 -0800

    Merge main into tong's personal branch

---
+++ components/Footer.js
@@ -0,0 +1,15 @@
+// components/Footer.js
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div>Info & Articles</div>
+      <div>Privacy & Terms</div>
+      <div>Contact Us</div>
+    </footer>
+  );
+}
+
+export default Footer;
