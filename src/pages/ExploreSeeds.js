8916fb8e0b812110eee9fc862b5304728434957c
Tong Guan <yuanyuanbingzi@gmail.com>
2024-11-24 22:24:01 -0800

    update ExploreSeeds with Uploading and Unlike Feature

--- src/pages/ExploreSeeds.js
+++ src/pages/ExploreSeeds.js
@@ -1,30 +1,31 @@
 import React, { useEffect, useState, useRef } from 'react';
 import p5 from 'p5';
-import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart as solidHeart } from '@fortawesome/free-solid-svg-icons'
+import { faHeart as solidHeart } from '@fortawesome/free-solid-svg-icons';
 import { faHeart as regularHeart } from '@fortawesome/free-regular-svg-icons';
 import './ExploreSeeds.css';
 import Navbar from '../components/Navbar.js';
 import Footer from '../components/Footer.js';
 import MiniNavbar from '../components/MiniNavbar.js';
+import LoadingAnimation from '../components/LoadingAnimation.js';
 import { WaveOscillator } from '../sketches/WaveOscillator.js';
 import { ConCirc } from '../sketches/concirc.js';
 import { Diagonals } from '../sketches/diags.js';
 import { Sslines } from '../sketches/sslines.js';
 import { TruchetRound } from '../sketches/truchetTriangles.js';
 import { LinesSketch } from '../sketches/lines.js';
-import { createLikeByParam, deleteLike} from '../apiclient/likes.js';
+import { createLikeByParam, deleteLike } from '../apiclient/likes.js';
 import { mapJWTToUserId } from '../apiclient/users.js';
 import { getArtworkById, getRecentArtworksWithLikes } from '../apiclient/artworks.js';
-import apiClient from '../apiclient/apiClient.js';
 
 const ExploreSeeds = () => {
-  const navigate = useNavigate();
   const [seedData, setSeedData] = useState([]);
+  const [filteredSeeds, setFilteredSeeds] = useState([]); 
   const [sortOrder, setSortOrder] = useState('newest');
-  const [searchQuery, setSearchQuery] = useState('');
   const [userId, setUserId] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [isSearching, setIsSearching] = useState(false);
   const [isLiking, setIsLiking] = useState(false);
   const canvasRefs = useRef({});
   const canvasSize = 200;
@@ -42,24 +43,30 @@ const ExploreSeeds = () => {
     fetchUserId();
   }, []);
 
-
   useEffect(() => {
     const fetchArtworks = async () => {
       try {
         const artworksWithLikes = await getRecentArtworksWithLikes();
-        setSeedData(artworksWithLikes);
+
+        // Loading Animation
+        setTimeout(() => {
+          setSeedData(artworksWithLikes);
+          handleFilterChange(sortOrder, artworksWithLikes); 
+          setIsLoading(false);
+        }, 2000);
       } catch (error) {
         console.error('Error fetching artworks:', error);
+        setIsLoading(false);
       }
     };
     fetchArtworks();
   }, []);
 
   useEffect(() => {
-    seedData.forEach((seed) => {
+    filteredSeeds.forEach((seed) => {
       createSketch(seed);
     });
-  }, [seedData]);
+  }, [filteredSeeds]);
 
   const createSketch = (artwork) => {
     const containerId = `seed-canvas-${artwork.id}`;
@@ -69,7 +76,7 @@ const ExploreSeeds = () => {
       return;
     }
 
-    // Clear previous sketches
+    // Clear Previous Sketches
     while (container.firstChild) {
       container.removeChild(container.firstChild);
     }
@@ -99,111 +106,151 @@ const ExploreSeeds = () => {
   };
 
   const handleSearch = (query) => {
-    setSearchQuery(query);  
+    setSearchQuery(query);
+    setIsSearching(true);
+
+    // Save time for the search result to draw
+    setTimeout(() => {
+      const filteredData = seedData.filter(
+        (seed) =>
+          seed.user?.name.toLowerCase().includes(query.toLowerCase()) ||
+          seed.algorithm?.toLowerCase().includes(query.toLowerCase())
+      );
+
+      setFilteredSeeds(filteredData); 
+      setIsSearching(false); 
+    }, 1500);
   };
 
-  const handleFilterChange = (filter) => {
+  const handleFilterChange = (filter, data = seedData) => {
     setSortOrder(filter);
+
+    let updatedFilteredSeeds = [...data];
     if (filter === 'mostLiked') {
-      setSeedData((prevData) => [...prevData].sort((a, b) => b.likes - a.likes));
+      updatedFilteredSeeds.sort((a, b) => b.likes - a.likes);
     } else if (filter === 'newest') {
-      setSeedData((prevData) => [...prevData].sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)));
+      updatedFilteredSeeds.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
     }
+
+    setFilteredSeeds(updatedFilteredSeeds);
   };
 
-  const filteredSeeds = seedData
-    .filter((seed) =>
-      seed.user?.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
-      seed.algorithm?.toLowerCase().includes(searchQuery.toLowerCase()) 
-    );
-
-  
   const handleLike = async (artworkId) => {
     if (!userId) {
-        alert('Please log in to like artworks.');
-        return;
+      alert('Please log in to like artworks.');
+      return;
     }
-
+  
     if (isLiking) {
-        return;
+      return; // Avoid repeated Liking
     }
-
-    console.log(`Handling like for artwork ID: ${artworkId}`);
+  
     setIsLiking(true);
-
+  
     try {
-        const artworkDetails = await getArtworkById(artworkId);
-        if (!artworkDetails) {
-            console.error('Failed to fetch artwork details.');
-            return;
+      const artwork = await getArtworkById(artworkId);
+      if (!artwork) {
+        console.error('Failed to fetch artwork details.');
+        return;
+      }
+  
+      const userLike = artwork.likes.find((like) => like.userId === userId);
+  
+      if (userLike) {
+        const Unlike = window.confirm(
+          'You have already liked this artwork. Do you want to unlike it?'
+        );
+  
+        if (Unlike) {
+          await deleteLike(userLike.id);
+          console.log(`Like removed for likeId: ${userLike.id}`);
+  
+          setTimeout(() => {
+            const updatedData = seedData.map((item) =>
+              item.id === artworkId
+                ? { ...item, userLiked: false, likes: item.likes - 1 }
+                : item
+            );
+            setSeedData(updatedData);
+            handleFilterChange(sortOrder, updatedData);
+          }, 500);
         }
-
-        // Check if the user already liked the artwork
-        const userAlreadyLiked = artworkDetails.likes.some((like) => like.userId === userId);
-        if (userAlreadyLiked) {
-            console.warn(`User already liked artwork with ID: ${artworkId}. Cannot like again.`);
-            alert('You have already liked this artwork.');
+      } else {
+        const response = await createLikeByParam(userId, artworkId);
+        if (response && response.id) {
+          console.log(`Like added for artworkId: ${artworkId}`);
+  
+          setTimeout(() => {
+            const updatedData = seedData.map((item) =>
+              item.id === artworkId
+                ? { ...item, userLiked: true, likes: item.likes + 1 }
+                : item
+            );
+            setSeedData(updatedData);
+            handleFilterChange(sortOrder, updatedData);
+          }, 500);
         } else {
-            const response = await createLikeByParam(userId, artworkId);
-            if (response && response.id) {
-                console.log(`Like added for artworkId: ${artworkId}`);
-                console.log(response);
-
-                setSeedData((prevData) =>
-                    prevData.map((item) =>
-                        item.id === artworkId ? { ...item, userLiked: true, likes: item.likes + 1 } : item
-                    )
-                );
-            } else {
-                console.error('Failed to create like on server. Response:', response);
-            }
+          console.error('Failed to create like on server.');
         }
+      }
     } catch (error) {
-        console.error('Failed to handle like operation:', error);
+      console.error('Failed to handle like/unlike operation:', error);
     } finally {
-        setIsLiking(false);
+      setIsLiking(false);
     }
-};
+  };
 
-
-return (
-  <div className="explore-seeds content">
-    <Navbar />
-    <MiniNavbar
-      onSearch={handleSearch}
-      onFilterChange={handleFilterChange}
-      currentFilter={sortOrder}/>
-    <div className="seeds-grid">
-    {filteredSeeds && filteredSeeds.length > 0 ? (filteredSeeds.map((seed) => (
-      <div className="seed-card" key={seed.id}>
-        <div
-          className="canvas-container"
-          id={`seed-canvas-${seed.id}`}
-          ref={(el) => (canvasRefs.current[`seed-canvas-${seed.id}`] = el)}/>
-        <div className="seed-info">
-          <div className="seed-info-header">
-            <p className="seed-author">Creator: {seed.user?.name.split(' ')[0]}</p>
-            <div className="like-section" onClick={() => handleLike(seed.id)}>
-              <FontAwesomeIcon icon={seed.userLiked ? solidHeart : regularHeart} className="like-icon" />
-              <span className="like-count">{seed.likes}</span>
-            </div>
-          </div>
-          <p className="seed-algo">Algorithm: {seed.algorithm}</p>
+  return (
+    <div className="explore-seeds content">
+      <Navbar />
+      <MiniNavbar
+        onSearch={handleSearch}
+        onFilterChange={handleFilterChange}
+        currentFilter={sortOrder}
+      />
+      {isLoading || isSearching ? ( 
+        <LoadingAnimation />
+      ) : (
+        <div className="seeds-grid">
+          {filteredSeeds && filteredSeeds.length > 0 ? (
+            filteredSeeds.map((seed) => (
+              <div className="seed-card" key={seed.id}>
+                <div
+                  className="canvas-container"
+                  id={`seed-canvas-${seed.id}`}
+                  ref={(el) => (canvasRefs.current[`seed-canvas-${seed.id}`] = el)}
+                />
+                <div className="seed-info">
+                  <div className="seed-info-header">
+                    <p className="seed-author">
+                      Creator: {seed.user?.name.split(' ')[0]}
+                    </p>
+                    <div
+                      className="like-section"
+                      onClick={() => handleLike(seed.id)}
+                    >
+                      <FontAwesomeIcon
+                        icon={seed.userLiked ? solidHeart : regularHeart}
+                        className="like-icon"
+                      />
+                      <span className="like-count">{seed.likes}</span>
+                    </div>
+                  </div>
+                  <p className="seed-algo">Algorithm: {seed.algorithm}</p>
+                </div>
+              </div>
+            ))
+          ) : (
+            <p>No seeds found...</p>
+          )}
         </div>
-      </div>
-    ))
-  ) : (
-    <p>No seeds found...</p>
-  )}
-</div>
-
-    <Footer />
-  </div>
-);
+      )}
+      <Footer />
+    </div>
+  );
 };
 
 export default ExploreSeeds;
-
 
 
 
